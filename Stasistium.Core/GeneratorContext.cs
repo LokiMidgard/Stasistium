2537ee180835d8d792eb30eac0ceaaaddbb5b208
Patrick Kranz <patrick-kranz@live.de>
2019-12-21 11:45:05 +0100

    Fix some warnings.

--- Stasistium.Core/GeneratorContext.cs
+++ Stasistium.Core/GeneratorContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Stasistium.Documents
 {
-    public class GeneratorContext : IDisposable
+    public sealed class GeneratorContext : IDisposable
     {
         private readonly HashAlgorithm algorithm = SHA256.Create();
         private readonly Func<object, string?>? objectToStingRepresentation;
@@ -121,44 +121,21 @@ namespace Stasistium.Documents
         #region IDisposable Support
         private bool disposedValue = false; // To detect redundant calls
 
-
-        protected virtual void Dispose(bool disposing)
+        public void Dispose()
         {
             if (!this.disposedValue)
             {
-                if (disposing)
-                {
-                    this.algorithm.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
+                this.algorithm.Dispose();
 
                 this.disposedValue = true;
             }
         }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~GeneratorContext()
-        // {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            this.Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
+        #endregion
 
         public Exception Exception(string message)
         {
             throw new NotImplementedException(message);
         }
-        #endregion
     }
 
 }
---
+++ Stasistium.Razor/RazorViewToStringRenderer.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.Extensions.FileProviders;
+using System.Collections.Generic;
+
+namespace Stasistium.Razor
+{
+    // The type is used to instantiate it.
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
+    internal class RazorViewToStringRenderer
+    {
+        private IRazorViewEngine _viewEngine;
+        private ITempDataProvider _tempDataProvider;
+        private IServiceProvider _serviceProvider;
+
+        public RazorViewToStringRenderer(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            this._viewEngine = viewEngine;
+            this._tempDataProvider = tempDataProvider;
+            this._serviceProvider = serviceProvider;
+        }
+
+        public async Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model)
+        {
+            var actionContext = this.GetActionContext();
+            var view = this.FindView(actionContext, viewName);
+
+            using (var output = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary<TModel>(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary())
+                    {
+                        Model = model
+                    },
+                    new TempDataDictionary(
+                        actionContext.HttpContext,
+                        this._tempDataProvider),
+                    output,
+                    new HtmlHelperOptions());
+
+                await view.RenderAsync(viewContext).ConfigureAwait(false);
+
+                return output.ToString();
+            }
+        }
+
+        private IView FindView(ActionContext actionContext, string viewName)
+        {
+            var getViewResult = this._viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage: true);
+            if (getViewResult.Success)
+            {
+                return getViewResult.View;
+            }
+
+            var findViewResult = this._viewEngine.FindView(actionContext, viewName, isMainPage: true);
+            if (findViewResult.Success)
+            {
+                return findViewResult.View;
+            }
+
+            var searchedLocations = getViewResult.SearchedLocations.Concat(findViewResult.SearchedLocations);
+            var errorMessage = string.Join(
+                Environment.NewLine,
+                new[] { $"Unable to find view '{viewName}'. The following locations were searched:" }.Concat(searchedLocations)); ;
+
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        internal static RazorViewToStringRenderer GetRenderer(IEnumerable<IFileProvider> fileProviders, string contentId)
+        {
+            var services = new ServiceCollection();
+            var applicationEnvironment = PlatformServices.Default.Application;
+            services.AddSingleton(applicationEnvironment);
+            
+            var environment = new HostingEnvironment
+            { 
+                ApplicationName =  Assembly.GetEntryAssembly().GetName().Name
+            };
+            services.AddSingleton<IHostingEnvironment>(environment);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.ViewLocationFormats.Clear();
+                options.PageViewLocationFormats.Clear();
+                options.ViewLocationFormats.Add($"/{contentId}/{{0}}");
+                options.PageViewLocationFormats.Add($"/{contentId}/{{0}}");
+                foreach (var provider in fileProviders)
+                    options.FileProviders.Add(provider);
+                    options.FileProviders.Add(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
+            });
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+
+            // We don't want to dispose something that is used as a singleton somewhere
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+#pragma warning restore CA2000 // Dispose objects before losing scope
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+
+            services.AddLogging();
+            services.AddMvc();
+            services.AddSingleton<RazorViewToStringRenderer>();
+            var provider = services.BuildServiceProvider();
+            return provider.GetRequiredService<RazorViewToStringRenderer>();
+        }
+
+
+        private ActionContext GetActionContext()
+        {
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = _serviceProvider
+            };
+            return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+        }
+    }
+}
