14d89e7c79c93c8891b3da606c1251c38c676dd6
Patrick Kranz <patrick-kranz@live.de>
2020-01-26 14:09:34 +0100

    Changed FileSystemStage to check also fileSystem changes

--- Stasistium.Core/Stages/FileSystemStage.cs
+++ Stasistium.Core/Stages/FileSystemStage.cs
@@ -1,49 +1,124 @@
-﻿using Stasistium.Documents;
+﻿using Stasistium.Core;
+using Stasistium.Documents;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Stasistium.Stages
 {
-    public class FileSystemStage<T> : GeneratedHelper.Multiple.Simple.OutputMultiSimpleInputSingle1List0StageBase<string, T, Stream>
+
+    public class FileSystemStage<T> : MultiStageBase<Stream, string, FileSystemCache<T>>
         where T : class
     {
-        public FileSystemStage(StagePerformHandler<string, T> inputSingle0, IGeneratorContext context, string? name) : base(inputSingle0, context, name)
+        private readonly StagePerformHandler<string, T> input;
+
+        public FileSystemStage(StagePerformHandler<string, T> input, IGeneratorContext context, string? name) : base(context, name)
         {
+            this.input = input;
         }
 
-        protected override Task<ImmutableList<IDocument<Stream>>> Work(IDocument<string> input, OptionToken options)
+        protected override async Task<StageResultList<Stream, string, FileSystemCache<T>>> DoInternal([AllowNull] FileSystemCache<T>? cache, OptionToken options)
         {
-            if (input is null)
-                throw new System.ArgumentNullException(nameof(input));
-            var path = input.Value;
-            var root = new DirectoryInfo(path);
+            var result = await this.input(cache?.PreviousCache, options).ConfigureAwait(false);
 
-            if (!root.Exists)
-                throw this.Context.Exception("Folder does not exists.");
-
-            var queue = new Queue<DirectoryInfo>();
-            queue.Enqueue(root);
-
-            var list = new List<FileInfo>();
-
-            while (queue.TryDequeue(out var directory))
+            var task = LazyTask.Create(async () =>
             {
-                list.AddRange(directory.GetFiles());
 
-                foreach (var subDirectory in directory.GetDirectories())
+                var (performed, newPreviousCache) = await result.Perform;
+
+
+                var path = performed.Value;
+                var root = new DirectoryInfo(path);
+
+                if (!root.Exists)
+                    throw this.Context.Exception("Folder does not exists.");
+
+                var queue = new Queue<DirectoryInfo>();
+                queue.Enqueue(root);
+
+                var list = new List<FileInfo>();
+
+                while (queue.TryDequeue(out var directory))
                 {
-                    queue.Enqueue(subDirectory);
+                    list.AddRange(directory.GetFiles());
+
+                    foreach (var subDirectory in directory.GetDirectories())
+                    {
+                        queue.Enqueue(subDirectory);
+                    }
                 }
-            }
 
 
-            return Task.FromResult(list.Select(x => new FileDocument(x, root, null, this.Context) as IDocument<Stream>).ToImmutableList());
+                var callculated = list.Select(file =>
+                {
+                    var id = Path.GetRelativePath(root.FullName, file.FullName);
+                    var writeTime = file.LastWriteTimeUtc;
+
+                    if (cache is null || !cache.PathToWriteTime.TryGetValue(id, out var lastWriteTime))
+                        lastWriteTime = default;
+                    var hasChanges = lastWriteTime != writeTime;
+
+                    if (hasChanges || cache is null)
+                    {
+                        // check if we actually have changes.
+                        var document = new FileDocument(file, root, null, this.Context);
+                        if (cache is null || !cache.PathToHash.TryGetValue(id, out string? lastHash))
+                            lastHash = null;
+
+                        hasChanges = document.Hash != lastHash;
+
+                        return (result: StageResult.Create(document as IDocument<Stream>, document.Hash, hasChanges, document.Id), writeTime, hash: document.Hash, id);
+                    }
+                    else
+                    {
+                        if (cache is null || !cache.PathToHash.TryGetValue(id, out string? lastHash))
+                            throw this.Context.Exception("Should not happpen");
+
+                        var subTask = LazyTask.Create(() =>
+                        {
+                            var document = new FileDocument(file, root, null, this.Context);
+                            return (document as IDocument<Stream>, document.Hash);
+                        });
+
+                        return (result: StageResult.Create(subTask, hasChanges, id), writeTime, hash: lastHash, id);
+                    }
+                }).ToArray();
+
+
+                var newCache = new FileSystemCache<T>()
+                {
+                    PreviousCache = newPreviousCache,
+                    PathToHash = callculated.ToDictionary(x => x.id, x => x.hash),
+                    PathToWriteTime = callculated.ToDictionary(x => x.id, x => x.writeTime),
+                    IdOrder = callculated.Select(x => x.id).ToArray()
+
+                };
+                var resultList = ImmutableList<StageResult<Stream, string>>.Empty.ToBuilder();
+                return (result: resultList.ToImmutable(), newCache);
+            });
+
+            var r = await task;
+            var hasChanges = r.result.Any(x => x.HasChanges)
+                                || cache is null
+                                || !cache.IdOrder.SequenceEqual(r.newCache.IdOrder);
+            var ids = r.newCache.IdOrder.ToImmutableList();
+
+            return StageResultList.Create(task, hasChanges, ids);
         }
     }
+
+    public class FileSystemCache<T>
+    {
+        public T PreviousCache { get; set; }
+        public Dictionary<string, DateTime> PathToWriteTime { get; set; }
+        public Dictionary<string, string> PathToHash { get; set; }
+        public string[] IdOrder { get; set; }
+    }
+
 
     internal class FileDocument : DocumentBase, IDocument<Stream>
     {
