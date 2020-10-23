fd2fe7e7db3da54b3ca3428459f32dff0b729eb3
Patrick Kranz <patrick-kranz@live.de>
2020-10-23 09:47:09 +0200

    Added ToList stage

---
+++ Stasistium.Core/Stages/ToListStage.cs
@@ -0,0 +1,39 @@
+﻿using Stasistium.Documents;
+using Stasistium.Stages;
+using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace Stasistium.Stages
+{
+    public class ToListStage<TIn, TPreviousCache> : GeneratedHelper.Multiple.Simple.OutputMultiSimpleInputSingle1List0StageBase<TIn, TPreviousCache, TIn>
+    where TPreviousCache : class
+    {
+        public ToListStage(StageBase<TIn, TPreviousCache> input, IGeneratorContext context, string? name = null) : base(input, context, name)
+        {
+        }
+
+        protected override Task<ImmutableList<IDocument<TIn>>> Work(IDocument<TIn> inputSingle0, OptionToken options)
+        {
+            return Task.FromResult(ImmutableList.Create(inputSingle0));
+        }
+    }
+
+
+}
+namespace Stasistium
+{
+
+
+    public static partial class StageExtensions
+    {
+
+        public static ToListStage<TCheck, TPreviousCache> ToList<TCheck, TPreviousCache>(this StageBase<TCheck, TPreviousCache> input, string? name = null)
+            where TPreviousCache : class
+        {
+            if (input is null)
+                throw new ArgumentNullException(nameof(input));
+            return new ToListStage<TCheck, TPreviousCache>(input, input.Context, name);
+        }
+    }
+}
