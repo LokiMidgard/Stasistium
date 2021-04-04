ccca07ef2b61aabf7ba3003b97dbb42717b1a3ce
Patrick Kranz <patrick-kranz@live.de>
2021-04-04 19:37:51 +0200

    Addedf CrossJoin and Zip

---
+++ Stasistium.Core/Stages/CrossJoinStage.cs
@@ -0,0 +1,25 @@
+﻿using Stasistium.Documents;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Stasistium.Stages
+{
+    public class CrossJoinStage<TInput, TAditional, TResult> : StageBase<TInput, TAditional, TResult>
+    {
+        protected override Task<ImmutableList<IDocument<TResult>>> Work(ImmutableList<IDocument<TInput>> input, ImmutableList<IDocument<TAditional>> additinoal, OptionToken options)
+        {
+            return Task.FromResult(input.SelectMany(x => additinoal.Select(y => this.transform(x, y))).ToImmutableList());
+        }
+
+
+        private readonly Func<IDocument<TInput>, IDocument<TAditional>, IDocument<TResult>> transform;
+
+
+        public CrossJoinStage(Func<IDocument<TInput>, IDocument<TAditional>, IDocument<TResult>> transform, IGeneratorContext context, string? name) : base(context, name)
+        {
+            this.transform = transform;
+        }
+    }
+}
---
+++ Stasistium.Core/Stages/ZipStage.cs
@@ -0,0 +1,25 @@
+﻿using Stasistium.Documents;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Stasistium.Stages
+{
+    public class ZipStage<TInput, TAditional, TResult> : StageBase<TInput, TAditional, TResult>
+    {
+        protected override Task<ImmutableList<IDocument<TResult>>> Work(ImmutableList<IDocument<TInput>> input, ImmutableList<IDocument<TAditional>> additinoal, OptionToken options)
+        {
+            return Task.FromResult(input.Zip(additinoal, (x, y) => this.transform(x, y)).ToImmutableList());
+        }
+
+
+        private readonly Func<IDocument<TInput>, IDocument<TAditional>, IDocument<TResult>> transform;
+
+
+        public ZipStage(Func<IDocument<TInput>, IDocument<TAditional>, IDocument<TResult>> transform, IGeneratorContext context, string? name) : base(context, name)
+        {
+            this.transform = transform;
+        }
+    }
+}
